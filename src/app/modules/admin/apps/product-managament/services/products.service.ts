9a1f3b9572eb4c4a0da83d9f067124a32dda67fe
BoutiMDG261 <ramiandra.marco@outlook.com>
2024-08-05 23:26:06 +0300

    story(A-003): Affichage produit et recherche

--- src/app/modules/admin/apps/product-managament/services/products.service.ts
+++ src/app/modules/admin/apps/product-managament/services/products.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Product } from '@app/core/models/product';
 import { PaginationData } from '@app/core/models/return.pagination';
@@ -23,8 +23,24 @@ export class ProductsService {
     return this._products.asObservable();
   }
 
-  getProduct(): Observable<ReturnTypes<PaginationData<Product>>> {
-    return this._httpClient.get<ReturnTypes<PaginationData<Product>>>(`${environment.api}/produit`, {})
+  getProduct(page: number, perPage: number): Observable<ReturnTypes<PaginationData<Product>>> {
+    const params = new HttpParams()
+      .set('page', page.toString())
+      .set('per_page', perPage.toString())
+
+    return this._httpClient.get<ReturnTypes<PaginationData<Product>>>(`${environment.api}/produit`, { params })
+      .pipe(
+        tap(products => {
+          this._products.next(products)
+        })
+      )
+  }
+
+  searchProduct(search: string): Observable<ReturnTypes<PaginationData<Product>>> {
+    const params = new HttpParams()
+      .set('key', search)
+
+      return this._httpClient.get<ReturnTypes<PaginationData<Product>>>(`${environment.api}/recherche`, { params })
       .pipe(
         tap(products => {
           this._products.next(products)
